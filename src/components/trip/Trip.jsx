2629211bb84a002a85bca7b86bef4413c4521fb2
Karima EL FILALI BOUKAI <karima.elfilaliboukai@alten.com>
2024-03-06 13:30:17 +0100

    select all trips

--- src/components/trip/Trip.jsx
+++ src/components/trip/Trip.jsx
@@ -2,8 +2,6 @@ import { react } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   TruckIcon,
-  CalendarDaysIcon,
-  UserIcon,
   ArchiveBoxIcon,
   ArrowsUpDownIcon,
   ChevronDoubleRightIcon,
@@ -13,33 +11,41 @@ import {
 import {
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Typography,
 } from "@material-tailwind/react";
 
-const Trip = (props) => {
+const Trip = ({ id, description, destinationId, originId, truckname, status }) => {
   const navigate = useNavigate();
-  const points = props.points;
+  const CalcStatusColor = (status) => {
+    switch (status) {
+      case "pending":
+        return false;
+      case "resolved":
+        return true;
+      default:
+        return null;
+    }
+  };
   return (
     <Card className="mt-8">
       <CardHeader className="bg-purple-100 flex items-center">
         <Typography variant="small" color="blue-gray" className="p-2">
-          12 500 DHS - 348 Kms
+          {description}
         </Typography>
       </CardHeader>
       <CardBody>
         <div className="trajet flex justify-between ">
           <div className="trajet-content w-3/4">
             <div className="traget-direction mb-5">
-              <div className="">{points.origin}</div>
+              <div>{originId}</div>
               <ArrowsUpDownIcon width="20px" height="20px" />
-              <div>{points.destination}</div>
+              <div>{destinationId}</div>
             </div>
             <div className="flex justify-between text-xs items-center">
               <div className="traget-material flex items-center w-1/2">
                 <TruckIcon width="20px" height="20px" className="mr-2" />
-                <span>IVECO - 2015</span>
+                <span>{truckname}</span>
               </div>
               <div className="trajet-info flex items-center w-1/2">
                 <ArchiveBoxIcon width="20px" height="20px" className="mr-2" />
@@ -49,11 +55,12 @@ const Trip = (props) => {
           </div>
           <div className="actions flex flex-col justify-between">
             <div className="etat-trajet">
-              {props.etat ? (
-                <PlayIcon width="25px" height="25px" fill="#2eaa35" />
-              ) : (
-                <PlayPauseIcon width="25px" height="25px" fill="#f8a72f" />
-              )}
+              {CalcStatusColor(status) ?
+                (
+                  <PlayIcon width="25px" height="25px" fill="#2eaa35" />
+                ) : (
+                  <PlayPauseIcon width="25px" height="25px" fill="#f8a72f" />
+                )}
             </div>
             <div className="detail-trajet">
               <ChevronDoubleRightIcon
@@ -61,7 +68,7 @@ const Trip = (props) => {
                 height="20px"
                 onClick={() => {
                   navigate(
-                    `/trajet-detail/?origin=${points.origin}&destination=${points.destination}`
+                    `/trajet-detail/?origin=${originId}&destination=${destinationId}`
                   );
                 }}
               />
