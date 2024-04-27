9ecc228f4ac9c844edf116c4392c907e4543d24d
ABDELILAH EL KRYMY <63428583+ABDELILAHelkrymy@users.noreply.github.com>
2024-04-27 19:05:44 +0100

    retours

--- src/components/navbar/MenuTrajet.jsx
+++ src/components/navbar/MenuTrajet.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./menu-trajet.css";
-const MenuTrajet = ({planifiedTrips}) => {
+const MenuTrajet = ({ planifiedTrips }) => {
   return (
     <>
       <div className="flex justify-between text-gray-700 rounded-xl shadow-md text-xs pt-2">
-        <div className="trajet-element rounded-l-lg">En cours (3)</div>
-        <div className="trajet-element is-active">Planifié ({planifiedTrips}+)</div>
+        <div className="trajet-element rounded-l-lg">En cours ()</div>
+        <div className="trajet-element is-active">
+          Planifié ({planifiedTrips ?? 0}
+          {planifiedTrips > 10 ? "+" : ""})
+        </div>
         <div className="trajet-element rounded-r-lg">Anciens</div>
       </div>
     </>
@@ -13,3 +16,4 @@ const MenuTrajet = ({planifiedTrips}) => {
 };
 
 export default MenuTrajet;
+
--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -18,10 +18,11 @@ export default function Upload({ entity, entityName, redirect }) {
   const dispatch = useDispatch();
   const [file, setFile] = useState("");
   const [name, setName] = useState("");
+  const [uploaded, setUploaded] = useState(false); // Add uploaded state
 
   const handleFileChange = (event) => {
-    console.log("File changed : ", event.target.files[0]);
     setFile(event.target.files[0]);
+    setUploaded(true); // Set uploaded state to true
   };
 
   const handleUpload = async () => {
@@ -50,10 +51,46 @@ export default function Upload({ entity, entityName, redirect }) {
   const clearInputs = () => {
     setFile("");
     setName("");
+    setUploaded(false); // Reset uploaded state to false
   };
+
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
+      {/* <input type="file" onChange={handleFileChange} /> */}
+      <label
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 "
+      >
+        <div className="flex flex-col items-center justify-center p-2">
+          <svg
+            className={`w-5 h-5 mb-2 ${uploaded ? "text-green-500" : "text-gray-500"}`} // Change color based on uploaded state
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 text-center">
+            <span className="font-semibold">
+              {uploaded ? file.name : "Cliquez pour télécharger"}
+            </span>{" "}
+            {/* Display file name if uploaded */}
+          </p>
+        </div>
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+      </label>
       <div className="mt-5">
         <Input
           label="Nom du fichier"
