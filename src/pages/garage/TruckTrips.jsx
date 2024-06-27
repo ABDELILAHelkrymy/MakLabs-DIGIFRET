04415e813fa38b3ca07767da865bf8bd7e007f0e
Abdelilah EL KRYMY <aelkrymy@provincelarache.local>
2024-06-27 12:23:54 +0200

    fix bugs related to truck trips by id

--- src/pages/garage/TruckTrips.jsx
+++ src/pages/garage/TruckTrips.jsx
@@ -1,42 +1,205 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { ChevronLeftIcon, ArrowUpTrayIcon } from "@heroicons/react/24/solid";
-import Trip from "../../components/trip/Trip";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { ChevronLeftIcon, ArrowUpTrayIcon } from "@heroicons/react/24/solid";
+// import Trip from "../../components/trip/Trip";
 
-const TruckTrips = () => {
+// const TruckTrips = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+
+//   return (
+//     <>
+//       {/* Page Header */}
+//       <div className="flex justify-between p-5 bg-white text-gray-700">
+//         <ChevronLeftIcon
+//           width="20px"
+//           height="20px"
+//           onClick={() => {
+//             navigate(`/garage-detail/${id}`);
+//           }}
+//         />
+//         <div className="">Liste des trajets</div>
+//         <ArrowUpTrayIcon width="25px" height="25px" fill="none" />
+//       </div>
+//       {/* Page Content  */}
+//       <div className="explore">
+//         <Trip
+//           etat={false}
+//           points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
+//         />
+//         <Trip
+//           etat={true}
+//           points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
+//         />
+//         <Trip
+//           etat={true}
+//           points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
+//         />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default TruckTrips;
+
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Trip from "../../components/trip/Trip";
+import MenuTrajet from "../../components/navbar/MenuTrajet";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  tripsSearch,
+} from "../../services/store/slices/tripsSlice";
+import Spinner from "../../components/spinner/Spinner";
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+const TripsPage = () => {
   const { id } = useParams();
+  const [tripsData, setTripsData] = useState([]);
+  const dispatch = useDispatch();
+  const { authData } = useSelector((state) => state.auth);
+  const { data, isLoading, error } = useSelector((state) => state.trips?.search);
+  const [planifiedTrips, setPlanifiedTrips] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(tripsSearch([
+      {
+        field: "truckId",
+        value: id,
+      },
+    ]));
+  }, []);
+
+  useEffect(() => {
+    if (data?.trips) {
+      const sortedTrips = [...data.trips].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+      console.log(sortedTrips); // This will show the sorted array
+      setTripsData(sortedTrips);
+      setPlanifiedTrips(data?.trips.length);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   if (data) {
+  //     const trips = data?.trips.sort((a, b) => {
+  //       return new Date(b.createdAt) - new Date(a.createdAt);
+  //     });
+  //     setTripsData(trips);
+  //     setPlanifiedTrips(trips?.length);
+  //   }
+  // }, [data]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (e.target.value === "") {
+      dispatch(tripsSearch([
+        {
+          field: "truckId",
+          value: id,
+        },
+      ]));
+    } else {
+      dispatch(
+        tripsSearch([
+          {
+            field: "description",
+            value: e.target.value,
+            operator: "regex",
+          },
+        ])
+      );
+    }
+  };
+
+  if (error) {
+    const errorMessage =
+      authData?.user?.role === "transporter"
+        ? "No Trucks in the garage under your responsability"
+        : "You don't have any truck in your garage";
+    <p className="text-red-500 text-center">{errorMessage}</p>;
+  }
 
   return (
     <>
-      {/* Page Header */}
-      <div className="flex justify-between p-5 bg-white text-gray-700">
-        <ChevronLeftIcon
-          width="20px"
-          height="20px"
-          onClick={() => {
-            navigate(`/garage-detail/${id}`);
-          }}
+      <p className="pageHeader">Trajets</p>
+      <div className="flex justify-between items-center p-2 bg-white text-gray-700">
+        <div className="">
+          <form className="max-w-md mx-auto">
+            <label
+              for="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Trouver un trajet
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Trouver un trajet"
+                onChange={(e) => handleSearch(e)}
+              />
+            </div>
+          </form>
+        </div>
+        <PlusIcon
+          width="25px"
+          height="25px"
+          fill="#2eaa35"
+          onClick={() => navigate("/add-trip")}
         />
-        <div className="">Liste des trajets</div>
-        <ArrowUpTrayIcon width="25px" height="25px" fill="none" />
       </div>
-      {/* Page Content  */}
-      <div className="explore">
-        <Trip
-          etat={false}
-          points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
-        />
-        <Trip
-          etat={true}
-          points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
-        />
-        <Trip
-          etat={true}
-          points={{ origin: "Fes, Morocco", destination: "Rabat, Morocco" }}
-        />
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <MenuTrajet planifiedTrips={planifiedTrips}/>
+          {tripsData &&
+            tripsData.map(
+              ({
+                _id,
+                description,
+                destinationId,
+                originId,
+                truckId,
+                status,
+                price,
+                distance,
+              }) => (
+                <Trip
+                  key={_id}
+                  description={description}
+                  originId={originId?.address}
+                  destinationId={destinationId?.address}
+                  truckname={truckId?.brand}
+                  status={status}
+                  price={price}
+                  distance={distance}
+                />
+              )
+            )}
+        </>
+      )}
     </>
   );
 };
 
-export default TruckTrips;
+export default TripsPage;
+
